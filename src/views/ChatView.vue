58d5711f73cd4610b66e6ced6bd26ac1f53e9dd5
dagmawibabi <1babidagi@gmail.com>
2024-03-25 20:13:20 +0300

    changed chat view

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,11 +1,12 @@
 <script setup>
-import { Send } from 'lucide-vue-next'
+import { Send, Trash } from 'lucide-vue-next'
 import { ref, reactive } from 'vue'
 import { OpenAI } from 'openai'
 import VueMarkdown from 'vue-markdown-render'
 
 const userInput = ref({ role: 'user', content: '' })
 const conversation = reactive([])
+const aiSystemPrompt = "Answer precisely and short but don't hold back on important details."
 
 function getUserInput() {
   userInput.value.content = document.getElementById('userInput').value
@@ -20,7 +21,6 @@ async function sendUserInputToAI() {
   })
   conversation.push(newUserInput)
   document.getElementById('userInput').value = ''
-  console.log(newUserInput.value.content)
 
   // SEND TO AI
   let newAIOutput = ref({
@@ -38,18 +38,25 @@ async function sendUserInputToAI() {
   let aiResponse = await openAI.chat.completions.create({
     model: 'local-model',
     messages: [
-      { role: 'system', content: 'Always reply short and precisely using a few words only!' },
+      {
+        role: 'system',
+        content: aiSystemPrompt
+      },
       { role: 'user', content: newUserInput.value.content.toString().trim() }
     ],
     stream: true
   })
-  let curRes = ''
   for await (const chunk of aiResponse) {
-    curRes += chunk.choices[0]?.delta?.content || ''
     newAIOutput.value.content += chunk.choices[0]?.delta?.content || ''
-    // console.log(chunk.choices[0]?.delta?.content)
   }
-  console.log(curRes)
+}
+
+function clearConversation() {
+  let responseLength = conversation.length
+  while (responseLength > 0) {
+    conversation.pop()
+    responseLength = conversation.length
+  }
 }
 </script>
 
@@ -57,8 +64,11 @@ async function sendUserInputToAI() {
   <div class="flex h-full justify-between">
     <!-- SETTINGS -->
     <div class="w-full h-full bg-[#202020]">
-      <div class="w-full border-b border-b-zinc-800 py-5">
+      <div class="w-full flex justify-between border-b border-b-zinc-800 py-5">
         <h1 class="uppercase pl-10 font-bold text-white">Chat With AI</h1>
+        <div class="pr-10 text-zinc-600 hover:text-zinc-300" @click="clearConversation">
+          <Trash :size="16" />
+        </div>
       </div>
       <div class="p-5">
         <!-- CHATS -->
@@ -70,19 +80,26 @@ async function sendUserInputToAI() {
             </span>
           </div>
           <div v-else>
-            <div v-for="messages in conversation" :key="messages.content" class="py-2">
+            <div v-for="messages in conversation" :key="messages.content" class="py-0">
+              <!-- USER INPUT -->
               <div
                 v-if="messages.value.role == 'user'"
-                class="bg-cyan-500 text-black font-bold rounded-full py-2 px-6"
+                class="bg-[#181818] text-white rounded-full py-2 px-6"
               >
                 {{ messages.value.content }}
               </div>
-              <div v-else class="bg-green-500 text-black font-bold rounded-3xl pt-2 pl-4 py-1 px-3">
-                <!-- {{ messages.value.content }} -->
-                <vue-markdown
-                  :source="messages.value.content"
-                  :option="{ typographer: true, hightlight: true }"
-                />
+              <!-- AI RESPONSES -->
+              <div v-else class="pb-10 pl-10">
+                <div
+                  class="w-4/5 bg-[#181818] drop-shadow-xl rounded-3xl rounded-tl-none text-green-500 pt-2 pb-3 pl-4 py-1 px-3 border-dashed border-l border-b border-l-zinc-700 border-b-zinc-800"
+                >
+                  <!-- <div v-else class="bg-zinc-900 text-green-500 rounded-3xl pt-2 pl-4 py-1 px-3"> -->
+                  <!-- {{ messages.value.content }} -->
+                  <vue-markdown
+                    :source="messages.value.content"
+                    :option="{ typographer: true, hightlight: true }"
+                  />
+                </div>
               </div>
             </div>
           </div>
@@ -97,6 +114,7 @@ async function sendUserInputToAI() {
               placeholder="Chat with AI..."
               @input="getUserInput"
               @keyup.enter="sendUserInputToAI"
+              autocomplete="off"
             />
           </div>
           <div class="pl-2 h-16">
@@ -128,14 +146,15 @@ async function sendUserInputToAI() {
 
 <style>
 p {
-  font-weight: bold;
+  /* font-weight: bold; */
+  padding-bottom: 10px;
 }
 pre {
-  background-color: #181818;
+  background-color: #202020;
   border-radius: 12px;
-  color: white;
-  padding: 16px;
-  margin-top: 5px;
+  color: rgb(3419794);
+  padding: 10px 20px 14px;
+  margin-top: 8px;
   margin-bottom: 5px;
   overflow-x: scroll;
 }
